ea5aba25a6fce96fc53b1ec32449523913b84c21
Scott M <gmat191@gmail.com>
2018-09-17 03:44:43 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,5 +38,9 @@ end
 
 def using_flatten(array)
   array.flatten 
+  #easy to think of as nested arrays and combining it's values 
 end
-  
+
+def using_delete(array, string)
+  array.delete(string)
+end
